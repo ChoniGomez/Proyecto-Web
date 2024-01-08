48b4962d0e99519347bef94ffee379a7dbb0aab7
ChoniGomez <chonidacharygomez@gmail.com>
2024-01-08 18:26:21 -0300

    Nuevos cambios con el Login del Usuario

--- Controllers/Usuarios.php
+++ Controllers/Usuarios.php
@@ -1,9 +1,35 @@
 <?php
 require_once('Config\App\Controller.php');
 class Usuarios extends Controller {
-    public function index(){
-        print_r($this->model->getUsuario());
+    public function __construct() {
+        session_start();
+        parent::__construct();
     }
 
+    public function index(){
+        $this->views->getView($this, "index");
+    }
+
+    public function validar() {
+        // si esta vacio el campo de usuario o clave
+        if (empty($_POST['usuario']) || empty($_POST['clave'])) {
+            $msg = "Los campos estan vacios";
+        }else{
+            $usuario = $_POST['usuario'];
+            $clave = $_POST['clave'];
+            $data = $this->model->getUsuario($usuario, $clave);
+            // verifico si existe o no un usuario
+            if ($data) {
+                $_SESSION['id_usuario'] = $data['id'];
+                $_SESSION['usuario'] = $data['usuario'];
+                $_SESSION['nombre'] = $data['nombre'];
+                $msg = "ok";
+            }else{
+                $msg = "Usuario o contraseña incorrecta";
+            }
+        }
+        echo json_encode($msg, JSON_UNESCAPED_UNICODE);
+        die();
+    }
 }
 ?>
--- Models/UsuariosModel.php
+++ Models/UsuariosModel.php
@@ -3,7 +3,12 @@ class UsuariosModel extends Query{
     public function __construct(){
         parent::__construct();
     }
-    public function getUsuario() {
+    public function getUsuario(string $usuario, string $clave) {
+        $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND clave = '$clave'";
+        $data = $this->select($sql);
+        return $data;
+    }
+    public function getUsuarios() {
         $sql = "SELECT * FROM usuarios";
         $data = $this->select($sql);
         return $data;
